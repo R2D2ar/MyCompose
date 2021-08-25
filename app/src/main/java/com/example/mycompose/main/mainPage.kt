b8e0203d1a2e64f9f88c7f093d187a9c1f76349e
R2D2 <rddeveloper31@gmail.com>
2021-08-25 11:51:34 +0200

    Added Scaffold to main

--- app/src/main/java/com/example/mycompose/main/mainPage.kt
+++ app/src/main/java/com/example/mycompose/main/mainPage.kt
@@ -3,31 +3,46 @@ package com.example.mycompose.main
 import android.annotation.SuppressLint
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.mycompose.LocalBackPressedDispatcher
+import com.example.mycompose.R
+import com.example.mycompose.ui.theme.Orange500
+import kotlinx.coroutines.launch
+
+
+enum class AnimationState{
+    START,
+    STOP
+}
+
 
 //MainPage
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun MainPage(navController: NavController, backPressedDispatcher: OnBackPressedDispatcher) {
     navController.enableOnBackPressed(false)
+    var animationState: AnimationState
+            by remember { mutableStateOf(AnimationState.STOP) }
 
+    val scope = rememberCoroutineScope()
     val scaffoldstate = rememberScaffoldState()
 
     val backDispatcher = LocalBackPressedDispatcher.current
@@ -48,6 +63,47 @@ fun MainPage(navController: NavController, backPressedDispatcher: OnBackPressedD
 
     Scaffold(
         scaffoldState = scaffoldstate,
+        topBar = {
+            TopAppBar(modifier = Modifier
+                .clip(RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)),
+                navigationIcon = {
+                Crossfade(targetState = animationState) {
+                    currentstate ->
+                    when(currentstate){
+                        AnimationState.START ->
+                            Icon(
+                                Icons.Default.Menu, null,
+                                modifier = Modifier
+                                    .padding(start = 10.dp, end = 10.dp)
+                                    .fillMaxSize()
+                                    .clickable(onClick = {
+                                        scope.launch {
+                                            //scaffoldstate.drawerState.open()
+                                            animationState = AnimationState.STOP
+
+                                        }
+                                    })
+                            )
+                        AnimationState.STOP ->
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_signal_cellular_connected_no_internet_4_bar_24), null,
+                                modifier = Modifier
+                                    .padding(start = 10.dp, end = 10.dp)
+                                    .fillMaxSize()
+                                    .clickable(onClick = {
+                                        scope.launch {
+                                            //scaffoldstate.drawerState.open()
+                                            animationState = AnimationState.START
+                                        }
+                                    })
+                            )
+                    }
+                }
+
+            }, title = {Text("TopAppBar")},
+            backgroundColor = Orange500)  },
+
+
         content = {MainContent()},
         drawerContent = { DrawerContent()}
     )
